33e25167631084b8b5f8d2be7fd3b9d6aab6f850
Justin Clark <justclark.jose@gmail.com>
2019-03-07 20:38:35 -0600

    added prompts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bamazon
+bamazon is an online decor store consisting of 10 items.
+
+Enter in the ID of the product that you would like to buy then you will be prompted to enter the quantity.
+Once the values have been entered, we will let you know if we can fulfill your request.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,3 +19,33 @@ connection.connect(function(err) {
   if (err) throw err;
   runSearch();
 });
+
+function decorSearch() {
+  inquirer
+    .prompt({
+      name: "id",
+      type: "input",
+      message: "Please enter the ID of the product you would like to buy?"
+    }, {
+      // How many would you like?
+      type: "input",
+      name: "stock  quantity",
+      message: "How many would you like to purchase? ",
+      // validate returns truef the entry is valid, otherwise throw error
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+
+
+    .then(function(answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runSearch();
+      });
+    })    });    };
